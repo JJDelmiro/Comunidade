9e82cdac42490fcf2d2e670b1fc281d9d637b187
JJDelmiro <132026425+JJDelmiro@users.noreply.github.com>
2026-08-22 02:14:00 -0300

    Main

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// ========================================
+// Comunidade Santa Brígida & Santa Rosa
+// JavaScript
+// ========================================
+
+
+// O site atualmente utiliza apenas
+// HTML e CSS.
+//
+// Este arquivo ficará reservado para
+// futuras interações da página.
